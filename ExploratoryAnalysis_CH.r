ee4dd63f1dbb134e31286c8a9668951097351a51
crhammon <crhammon@users.noreply.github.com>
2021-01-18 19:23:24 -0800

    Added plot

--- ExploratoryAnalysis_CH.r
+++ ExploratoryAnalysis_CH.r
@@ -1,2 +1,7 @@
+library(ggplot2)
+
 data("Fatalities",package = 'AER')
 # https://www.rdocumentation.org/packages/AER/versions/1.2-9/topics/Fatalities
+
+ggplot(data = Fatalities) +
+  geom_point(mapping = aes(x=state, y = nfatal,size = beertax))
