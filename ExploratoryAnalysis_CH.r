ab0de1d01b0f2082ae3447e2a2907349cbe3bdec
crhammon <crhammon@users.noreply.github.com>
2021-02-04 11:52:47 -0800

    update explore

--- ExploratoryAnalysis_CH.r
+++ ExploratoryAnalysis_CH.r
@@ -8,14 +8,18 @@ data("Fatalities",package = 'AER')
 
 # pairs(Fatalities[,c('fatal','emppop','youngdrivers','mormon','baptist')])
 
-
 # plotmeans(afatal/pop~dry, data = Fatalities)
 # plotmeans(fatal/pop~drinkage, data = Fatalities)
+
 plotmeans(afatal/pop~breath, data = Fatalities) # bingo
-# plotmeans(fatal2124/pop~drinkage, data = Fatalities) # bingo
 
-interaction.plot(Fatalities$breath, Fatalities$jail, Fatalities$afatal/Fatalities$pop*100000) # bingo
+# interaction.plot(Fatalities$breath, Fatalities$jail, Fatalities$afatal/Fatalities$pop*100000) # bingo
 
-# interaction.plot(Fatalities$breath, Fatalities$service, Fatalities$afatal/Fatalities$pop*100000) # bingo
+hist(Fatalities$pop)
+# GroupALogical = Fatalities$pop <
+# GroupA = Fatalities[GroupALogical,]
 
-# plotmeans(afatal/pop*1e5~dry, data = Fatalities)
+
+
+
+
