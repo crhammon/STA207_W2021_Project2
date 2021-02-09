9d291a734e2de29eb8ce5b4d1ef1dac63437d444
crhammon <crhammon@users.noreply.github.com>
2021-02-08 20:11:28 -0800

    added fixed effects model

--- ExploratoryAnalysis_CH.r
+++ ExploratoryAnalysis_CH.r
@@ -1,5 +1,8 @@
 library(ggplot2)
 library(gplots)
+library(plm)
+library(clubSandwich)
+
 data("Fatalities",package = 'AER')
 # https://www.rdocumentation.org/packages/AER/versions/1.2-9/topics/Fatalities
 
@@ -13,13 +16,37 @@ data("Fatalities",package = 'AER')
 
 plotmeans(afatal/pop~breath, data = Fatalities) # bingo
 
-# interaction.plot(Fatalities$breath, Fatalities$jail, Fatalities$afatal/Fatalities$pop*100000) # bingo
-
-hist(Fatalities$pop)
-# GroupALogical = Fatalities$pop <
-# GroupA = Fatalities[GroupALogical,]
+interaction.plot(Fatalities$breath, Fatalities$jail, Fatalities$afatal/Fatalities$pop*100000) # bingo
 
 
+## initial analysis
 
+Fatalities$afatalper100k = Fatalities$afatal/Fatalities$pop * 1e5
+
+plotmeans(afatalper100k~jail, data = Fatalities) # bingo
+
+# https://www.econometrics-with-r.org/10-3-fixed-effects-regression.html
+# from ^^
+# estimate the fixed effects regression with plm()
+
+FE_OLS = plm(afatalper100k ~ spirits +
+                             beertax +
+                             drinkage +
+                             breath +
+                             jail + 
+                             service +
+                             as.factor(state),
+                    data = Fatalities,
+                    index = c("state", "year"), 
+                    model = "within",
+                    effect = 'twoway')
+
+
+coef_test(FE_OLS, vcov = "CR1", cluster = "individual", test = "naive-t")
+coef_test(FE_OLS, vcov = "CR1", cluster = "individual", test = "Satterthwaite")
+
+
+
+# plot(FE_OLS, which = 1)
 
 
